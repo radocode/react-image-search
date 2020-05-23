a9ad11fa1b5998916412d5d58dc4157be6f31920
Francisco González <franciscogonzalez@MacBook-Pro.local>
2020-05-22 22:27:02 -0400

    Updates

---
+++ precache-manifest.fb2a11aa52e65bcb4447883198be547f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3bab33b9d526c6825ec15db0ab47561",
+    "url": "/react-image-search/index.html"
+  },
+  {
+    "revision": "cf8ebb38976afe195127",
+    "url": "/react-image-search/static/css/main.3a51ddc4.chunk.css"
+  },
+  {
+    "revision": "84c8b563ac1fd5268b35",
+    "url": "/react-image-search/static/js/2.3dab0930.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-image-search/static/js/2.3dab0930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf8ebb38976afe195127",
+    "url": "/react-image-search/static/js/main.55be4ff9.chunk.js"
+  },
+  {
+    "revision": "4719b6b174f8b394a278",
+    "url": "/react-image-search/static/js/runtime-main.c5c31916.js"
+  }
+]);
